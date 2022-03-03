93744ec58cc5f1fa57795e3dd4829ca91e504dff
Cristofer <churisalex91@gmail.com>
2022-03-03 13:46:41 -0500

    nuevo

---
+++ MayorEdad (1).js
@@ -0,0 +1,190 @@
+// Este codigo ha sido generado por el modulo psexport 20180802-w32 de PSeInt.
+// Es posible que el codigo generado no sea completamente correcto. Si encuentra
+// errores por favor reportelos en el foro (http://pseint.sourceforge.net).
+
+function mayoredad() {
+	var conteo;
+	var edad1 = new Number();
+	var edad2 = new Number();
+	var edad3 = new Number();
+	var edad4 = new Number();
+	var edad5 = new Number();
+	var edad6 = new Number();
+	var edad7 = new Number();
+	var edad8 = new Number();
+	var edad9 = new Number();
+	var edad10 = new Number();
+	var edad11 = new Number();
+	var edad12 = new Number();
+	var edad13 = new Number();
+	var edad14 = new Number();
+	var edad15 = new Number();
+	var edad16 = new Number();
+	var edad17 = new Number();
+	var edad18 = new Number();
+	var edad19 = new Number();
+	var edad20 = new Number();
+	document.write("Ingrese la edad de la primera persona",'<BR/>');
+	edad1 = Number(prompt());
+	document.write("Ingrese la edad de la segunda persona",'<BR/>');
+	edad2 = Number(prompt());
+	document.write("Ingrese la edad de la terdera persona",'<BR/>');
+	edad3 = Number(prompt());
+	document.write("Ingrese la edad de la primera persona",'<BR/>');
+	edad4 = Number(prompt());
+	document.write("Ingrese la edad de la primera persona",'<BR/>');
+	edad5 = Number(prompt());
+	document.write("Ingrese la edad de la primera persona",'<BR/>');
+	edad6 = Number(prompt());
+	document.write("Ingrese la edad de la primera persona",'<BR/>');
+	edad7 = Number(prompt());
+	document.write("Ingrese la edad de la primera persona",'<BR/>');
+	edad8 = Number(prompt());
+	document.write("Ingrese la edad de la primera persona",'<BR/>');
+	edad9 = Number(prompt());
+	document.write("Ingrese la edad de la primera persona",'<BR/>');
+	edad10 = Number(prompt());
+	document.write("Ingrese la edad de la persona 11 ",'<BR/>');
+	edad11 = Number(prompt());
+	document.write("Ingrese la edad de la  persona 12",'<BR/>');
+	edad12 = Number(prompt());
+	document.write("Ingrese la edad de la  persona 13",'<BR/>');
+	edad13 = Number(prompt());
+	document.write("Ingrese la edad de la  persona 14",'<BR/>');
+	edad14 = Number(prompt());
+	document.write("Ingrese la edad de la  persona 15 ",'<BR/>');
+	edad15 = Number(prompt());
+	document.write("Ingrese la edad de la  persona 16",'<BR/>');
+	edad16 = Number(prompt());
+	document.write("Ingrese la edad de la  persona 17",'<BR/>');
+	edad17 = Number(prompt());
+	document.write("Ingrese la edad de la  persona 18",'<BR/>');
+	edad18 = Number(prompt());
+	document.write("Ingrese la edad de la  persona 19",'<BR/>');
+	edad19 = Number(prompt());
+	document.write("Ingrese la edad de la  persona 20",'<BR/>');
+	edad20 = Number(prompt());
+	conteo = 0;
+	if ((edad1>=18)) {
+		document.write("La primera persona es mayor de edad con ",edad1,"Años",'<BR/>');
+		conteo = conteo+1;
+	} else {
+		document.write("Decir la primera persona es menor de edad ",edad1," Años ",'<BR/>');
+	}
+	if ((edad2>=18)) {
+		document.write("La segunda persona es mayor de edad con ",edad2,"Años",'<BR/>');
+		conteo = conteo+1;
+	} else {
+		document.write("Decir la segunda persona es menor de edad ",edad2," Años ",'<BR/>');
+	}
+	if ((edad3>=18)) {
+		document.write("La tercera persona es mayor de edad con ",edad3,"Años",'<BR/>');
+		conteo = conteo+1;
+	} else {
+		document.write("Decir la tercera persona es menor de edad ",edad3," Años ",'<BR/>');
+	}
+	if ((edad4>=18)) {
+		document.write("La cuarta persona es mayor de edad con ",edad4,"Años",'<BR/>');
+		conteo = conteo+1;
+	} else {
+		document.write("Decir la cuarta persona es menor de edad ",edad4," Años ",'<BR/>');
+	}
+	if ((edad5>=18)) {
+		document.write("La quinta persona es mayor de edad con ",edad5,"Años",'<BR/>');
+		conteo = conteo+1;
+	} else {
+		document.write("Decir la quinta persona es menor de edad ",edad5," Años ",'<BR/>');
+	}
+	if ((edad6>=18)) {
+		document.write("La sexta persona es mayor de edad con ",edad6,"Años",'<BR/>');
+		conteo = conteo+1;
+	} else {
+		document.write("Decir la sexta persona es menor de edad ",edad6," Años ",'<BR/>');
+	}
+	if ((edad7>=18)) {
+		document.write("La septima persona es mayor de edad con ",edad7,"Años",'<BR/>');
+		conteo = conteo+1;
+	} else {
+		document.write("Decir la septima persona es menor de edad ",edad7," Años ",'<BR/>');
+	}
+	if ((edad8>=18)) {
+		document.write("La octava persona es mayor de edad con ",edad8,"Años",'<BR/>');
+	} else {
+		document.write("Decir la octava persona es menor de edad ",edad8," Años ",'<BR/>');
+		conteo = conteo+1;
+	}
+	if ((edad9>=18)) {
+		document.write("La novena persona es mayor de edad con ",edad9,"Años",'<BR/>');
+		conteo = conteo+1;
+	} else {
+		document.write("Decir la novena persona es menor de edad ",edad9," Años ",'<BR/>');
+	}
+	if ((edad10>=18)) {
+		document.write("La decima persona es mayor de edad con ",edad10,"Años",'<BR/>');
+		conteo = conteo+1;
+	} else {
+		document.write("Decir la decima persona es menor de edad ",edad10," Años ",'<BR/>');
+	}
+	if ((edad11>=18)) {
+		document.write("La  persona 11 es mayor de edad con ",edad11,"Años",'<BR/>');
+		conteo = conteo+1;
+	} else {
+		document.write("Decir la  persona 11 es menor de edad ",edad11," Años ",'<BR/>');
+	}
+	if ((edad12>=18)) {
+		document.write("La  persona 12 es mayor de edad con ",edad12,"Años",'<BR/>');
+		conteo = conteo+1;
+	} else {
+		document.write("Decir la  persona 12 es menor de edad ",edad12," Años ",'<BR/>');
+	}
+	if ((edad13>=18)) {
+		document.write("La  persona 13 es mayor de edad con ",edad13,"Años",'<BR/>');
+		conteo = conteo+1;
+	} else {
+		document.write("Decir la  persona 13 es menor de edad ",edad13," Años ",'<BR/>');
+	}
+	if ((edad14>=18)) {
+		document.write("La  persona 14 es mayor de edad con ",edad14,"Años",'<BR/>');
+		conteo = conteo+1;
+	} else {
+		document.write("Decir la  persona 14 es menor de edad ",edad14," Años ",'<BR/>');
+	}
+	if ((edad15>=18)) {
+		document.write("La primera persona es mayor de edad con ",edad15,"Años",'<BR/>');
+		conteo = conteo+1;
+	} else {
+		document.write("Decir la primera persona es menor de edad ",edad15," Años ",'<BR/>');
+	}
+	if ((edad16>=18)) {
+		document.write("La  persona 16 es mayor de edad con ",edad16,"Años",'<BR/>');
+		conteo = conteo+1;
+	} else {
+		document.write("Decir la  persona 16 es menor de edad ",edad16," Años ",'<BR/>');
+	}
+	if ((edad17>=18)) {
+		document.write("La  persona 17es mayor de edad con ",edad17,"Años",'<BR/>');
+		conteo = conteo+1;
+	} else {
+		document.write("Decir la persona 17 es menor de edad ",edad17," Años ",'<BR/>');
+	}
+	if ((edad18>=18)) {
+		document.write("La  persona 18 es mayor de edad con ",edad18,"Años",'<BR/>');
+		conteo = conteo+1;
+	} else {
+		document.write("Decir la  persona 18 es menor de edad ",edad18," Años ",'<BR/>');
+	}
+	if ((edad19>=18)) {
+		document.write("La  persona 19 es mayor de edad con ",edad19,"Años",'<BR/>');
+		conteo = conteo+1;
+	} else {
+		document.write("Decir la  persona  19 es menor de edad ",edad19," Años ",'<BR/>');
+	}
+	if ((edad20>=18)) {
+		document.write("La  persona 20 es mayor de edad con ",edad20,"Años",'<BR/>');
+		conteo = conteo+1;
+	} else {
+		document.write("Decir la  persona 20 es menor de edad ",edad20," Años ",'<BR/>');
+	}
+	conteo = conteo+1;
+}
+
